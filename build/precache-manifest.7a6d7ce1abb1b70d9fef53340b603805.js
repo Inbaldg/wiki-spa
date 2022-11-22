db242f8bd513e01aa52e622d20415d0e15289ca1
Inbal de Groot <inbaldg19@gmail.com>
2022-11-22 19:32:25 +0000

    Update 16 files

---
+++ build/precache-manifest.7a6d7ce1abb1b70d9fef53340b603805.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d22418b57185208980ed50d354d33a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4af0a12728ac8c20f662",
+    "url": "/static/js/2.5ebb5ab0.chunk.js"
+  },
+  {
+    "revision": "ce66e740a698c8e43428971e98ca7007",
+    "url": "/static/js/2.5ebb5ab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "120f1a2e80dc0185a1a8",
+    "url": "/static/js/main.632f5081.chunk.js"
+  },
+  {
+    "revision": "a51a16ed7e2ccc23d480",
+    "url": "/static/js/runtime-main.4573ac71.js"
+  }
+]);
